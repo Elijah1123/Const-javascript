3be769c8e0d8b5de589bffa514d191b7a52a3839
elijah1123 <elijahmzalendo659@gmail.com>
2026-02-23 12:42:23 +0300

    const

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ let circumference;
 // circumference = 2 * PI * radius;
 // console.log( circumference)
 
-document.getElementById("mySubmit").onClick = function(){
+document.getElementById("mySubmit").onclick = function(){
     radius = document.getElementById("myText").value;
     radius = Number(radius);
     circumference = 2 * PI * radius;
